25850e035add275e0e611b874d6335fc66c5f945
Pedro Henrique Candido <pedrohecan@gmail.com>
2024-10-08 08:48:49 -0300

    Python

---
+++ conjuntos.py
@@ -0,0 +1,9 @@
+from random import randint
+a = 0
+Qtde = int(input('Digite a quantidade: '))
+conjunto = set()
+while len(conjunto) < Qtde:
+    conjunto.add(randint(a:1, b:50))
+print(conjunto)
+print(f'O conjunto tem {len(conjunto)} elementos')
+print("\nFim do programa")
